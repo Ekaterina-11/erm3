f98ba87e2b18fe3c7f29120919c735e2614355ce
Ekaterina-11 <62566219+Ekaterina-11@users.noreply.github.com>
2020-04-23 22:26:20 +0300

    Update stol.cpp

--- stol.cpp
+++ stol.cpp
@@ -1,18 +1,25 @@
 #include "stol.h"
 
-stol::stol(string name_, int size_, string color_, string material_):mebel(name_, size_, color_, material_){
+Table::Table(string const &name_, int const size_, string const &color_, string const &material_):mebel(name_, size_, color_, material_){
 }
 
-stol::stol(const stol &p){
+Table::Table(const Table &p){
     name = p.name;
     size = p.size;
     color = p.color;
     material = p.material;
 }
 
-void stol::print() const{
-cout << "Nazvanie: " << name << endl;
-cout << "Razmer: " << size << endl;
-cout << "Cvet: " << color << endl;
+void Table::print() const{
+cout << "Name: " << name << endl;
+cout << "Size: " << size << endl;
+cout << "Colour: " << color << endl;
 cout << "Material: " << material << endl;
+}
+
+Table& Table::operator = (Table const &p)
+{
+    mebel::operator=(p);
+    this->kolsec =p.kolsec;
+    return *this;
 }
