df888285b3abf80baef9e977cb2fc96f0e1357d5
Ekaterina-11 <62566219+Ekaterina-11@users.noreply.github.com>
2020-04-23 22:15:25 +0300

    Update divan.h

--- divan.h
+++ divan.h
@@ -7,13 +7,14 @@ class divan: public mebel{
     string pokritie;
 public:
     divan();
-    divan(string name_, int size_, string color_, string material_, string pokritie_);
+    divan(string const &name_, int const size_, string const &color_, string const &material_, string const &pokritie_);
     divan(const divan &p);
 
     bool SetPokritie(int pokritie_);
-    string GetPokritie() const;
+    const string& GetPokritie() const;
 
     void print() const;
+    divan& operator = (divan const &p);
 };
 
-#endif // SHKAF_H_INCLUDED
+#endif
